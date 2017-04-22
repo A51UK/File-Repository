747bb2affa939227eb78e2713fd20bd4abeaf717
Craig Adams <lord.a51.uk@gmail.com>
2017-04-22 10:37:31 +0100

    Added a test project for  File-Reposiory Project.

---
+++ File-Repository-Tests/AzureFileStorageTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ile_Repository_Tests
+{
+    [TestClass]
+    public class AzureFileStorageTests
+    {
+    }
+}
---
+++ File-Repository-Tests/LocalStorageFileSystemTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace File_Repository_Tests
+{
+    [TestClass]
+    public class LocalStorageFileSystemTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
