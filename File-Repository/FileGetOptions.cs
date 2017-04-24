5276a071bf57f04389c4db4857fb47bdf6afd839
Craig Adams <lord.a51.uk@gmail.com>
2017-04-24 14:46:29 +0100

    Update FileGetOptions.cs

--- File-Repository/FileGetOptions.cs
+++ File-Repository/FileGetOptions.cs
@@ -1,5 +1,5 @@
 ﻿/*
- *  Copyright 2017 Criag Lee Mark Adams
+ *  Copyright 2017 Craig Lee Mark Adams
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
